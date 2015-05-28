2c7efc700cd2b82b42f49e4828a950c20ae9fc7f
Brock Riemenschneider <bswim777@aim.com>
2015-05-28 17:12:38 -0500

    Don't create gray scale svg images

--- grayscale.php
+++ grayscale.php
@@ -5,9 +5,9 @@
  * Description: Automatically create grayscale thumbnails for a given image size
  * Author: Fabien Quatravaux
  * Version: 1.2
- 
+
   Copyright © 2012  Fabien Quatravaux  (email : fabien.quatravaux@1nterval.com)
-    
+
     Grayscale is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -22,7 +22,7 @@
     along with Grayscale.  If not, see <http://www.gnu.org/licenses/>
 */
 
-register_activation_hook(__FILE__, 'grayscale_check'); 
+register_activation_hook(__FILE__, 'grayscale_check');
 function grayscale_check() {
     if ( !extension_loaded('gd') ) {
         die("La librairie php GD est nécessaire pour ce plugin. Il semble qu'elle ne soit pas installée.");
@@ -52,35 +52,39 @@ function grayscale_check_grayscale_image($metadata, $attachment_id){
     global $_wp_additional_image_sizes;
     $attachment = get_post( $attachment_id );
     if ( preg_match('!image!', get_post_mime_type( $attachment )) ) {
-    
+
         require_once('class-grayscale-image-editor.php');
-        
+
         foreach($_wp_additional_image_sizes as $size => $size_data){
             if(isset($size_data['grayscale']) && $size_data['grayscale']) {
-                if(is_array($metadata['sizes']) && isset($metadata['sizes'][$size])){
+                $filename = false;
+
+                if(isset($metadata['sizes']) && is_array($metadata['sizes']) && isset($metadata['sizes'][$size])){
                     $file = pathinfo(get_attached_file($attachment_id));
                     $filename = $file['dirname'].'/'.$metadata['sizes'][$size]['file'];
                     $metadata['sizes'][$size.'-gray'] = $metadata['sizes'][$size];
-                } else {
+                } else if (isset($metadata['sizes']) && is_array($metadata['sizes'])) {
                     // this size has no image attached, probably because the original is too small
                     // create the grayscale image from the original file
                     $file = wp_upload_dir();
                     $filename = $file['basedir'].'/'.$metadata['file'];
                     $metadata['sizes'][$size.'-gray'] = array(
-                        'width' => $metadata['width'], 
+                        'width' => $metadata['width'],
                         'height' => $metadata['height'],
                     );
                 }
-                
-                $image = new Grayscale_Image_Editor($filename);
-                $image->load();
-                $image->make_grayscale();
-                $result = $image->save($image->generate_filename('gray'));
-                $metadata['sizes'][$size.'-gray']['file'] = $result['file'];
+
+                if ($filename) {
+                    $image = new Grayscale_Image_Editor($filename);
+                    $image->load();
+                    $image->make_grayscale();
+                    $result = $image->save($image->generate_filename('gray'));
+                    $metadata['sizes'][$size.'-gray']['file'] = $result['file'];
+                }
             }
         }
     }
     return $metadata;
 }
-    
+
 ?>
